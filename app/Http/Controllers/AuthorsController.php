bcda706025233bbc60e64b285b4399a9f95b635c
Fernando Lamas <lamas.fernando1419@gmail.com>
2019-03-13 18:31:09 -0300

    Adapting fields from DB to what will be exposed to the API

--- app/Http/Controllers/AuthorsController.php
+++ app/Http/Controllers/AuthorsController.php
@@ -9,9 +9,40 @@ class AuthorsController extends Controller
     public function index()
     {
         $authors = Author::all();
-        
+     
         return response()->json([
-            'data' => $authors 
+            'data' => $this->transformCollection($authors) 
         ], 200);
     }
+
+    /**
+     * transformCollection
+     *
+     * @param mixed $authors
+     * @return void
+     */
+    private function transformCollection($authors)
+    {        
+        return array_map([$this, 'transform'], $authors->toArray());
+    }
+
+    /**
+     * transform
+     *
+     * @param mixed $author
+     * @return void
+     */
+    private function transform($author)
+    {
+        // The apis will work on the keys rather than the name of the fields in authors DB table, that is why tranform is necessary.
+        return [
+            'name'         => $author['name'],
+            'email'        => $author['email'],
+            'github'       => $author['github'],
+            'twitter'      => $author['twitter'],
+            'last_article' => $author['last_article_published'],
+            'active'       => (boolean) $author['some_boolean']
+        ]; 
+    }
+
 }
