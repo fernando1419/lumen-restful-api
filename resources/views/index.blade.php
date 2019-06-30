1ceaf41daa8ead1e98de795db53e6327ba9e92f7
Fernando Lamas <lamas.fernando1419@gmail.com>
2019-06-29 23:49:42 -0300

    closes #10 - Adding bulma.io framework for styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,28 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layout')
 
-    <h2> Restfull Api developed using the Lumen Framework </h2>
+@section('content')
+    <div class="notification has-background-grey-light is-size-5 has-text-left">
+        <strong> Restfull Api developed using the Lumen Framework. </strong>
+    </div>
 
-    <h4>
-        <p>
-            <ul>
-                <li style="margin-bottom:20px;"> This API is created for handling requests to Authors and Books entities. For the time being only the API for handling "Authors" has been developed. </li>
-                <li style="margin-bottom:20px;"> Over the near term, I will be working on the pending "Books" API as also in a front-end development to consume these APIs. </li>
-                <li style="margin-bottom:20px;"> I will be using VueJS or Angular programming language for client-side development. </li>
-            </ul>
-        </p>
-    </h4>
+    <div>
+        <li style="margin-bottom:20px;"> This API is created for handling requests to Authors and Books entities. For the time being only the API for handling "Authors" has been developed. </li>
+        <li style="margin-bottom:20px;"> Over the near term, I will be working on the pending "Books" API as also in a front-end development to consume these APIs. </li>
+        <li style="margin-bottom:20px;"> I will be using VueJS or Angular programming language for client-side development. </li>
+    </div>
+@endsection
 
-    <br/>
+@section('footer')
+    <div class="notification has-background-grey-lighter">
+        Please refer to the <strong> <a href="https://documenter.getpostman.com/view/7918488/S1a32Sdb?version=latest" target="_blank">
+        API Documentation </a> </strong> for instructions on how to use it.
+    </div>
+@endsection
 
-    <h3> Please refer to the <a href="https://documenter.getpostman.com/view/7918488/S1a32Sdb?version=latest" target="_blank"> API Documentation </a> for instructions on how to use them. </h3>
 
-</body>
-</html>
+
+
+
